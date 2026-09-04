b1b5befab3b91d49cb489fe85990bdfc58979fb9
Sahil Ahmad <hello.sahilahmad@gmail.com>
2026-09-04 13:58:08 +0000

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'shilpsetu-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/before.png',
+  '/after.png',
+  '/icons/icon-128.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
